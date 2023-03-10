201b5e53d840e8f64524deedf04f3e6b3768cf10
Yisak Girmay <yisakbruck23@gmail.com>
2023-03-10 02:05:14 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+const names = []
+const event = "suprise"
+
+
+
+
+const writeCards = (arr,event) => {
+    const messages = []
+    for(let i = 0; i < arr.length;i++) {
+        
+           const greetingMessage = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+           messages.push(greetingMessage)
+          
+    }
+   
+            return messages;
+}
+
+let number = 0
+
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+  }
+}
